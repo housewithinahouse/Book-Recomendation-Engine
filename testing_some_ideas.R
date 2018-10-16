bee0eac573bb6486d02d6041f9a555643e1f6b56
Edwin Fallwell <efallwel@gmail.com>
2018-10-16 15:29:48 -0400

    just starting out

--- testing_some_ideas.R
+++ testing_some_ideas.R
@@ -1,3 +1,7 @@
-
+install.packages("dplyr")
+library("dplyr")
+book_tags_more_than_fifty = filter(book_tags, count > 50)
+book_tags <- read_csv("GitHub/goodbooks-10k/book_tags.csv")
+tags <- read_csv("GitHub/goodbooks-10k/tags.csv")
 book_tags_more_than_fifty = filter(book_tags, count > 50)
 book_tags_more_than_fifty["tag_id"] <- tags[ match(book_tags_more_than_fifty[['tag_id']], tags[['tag_id']] ) , 'tag_name']
